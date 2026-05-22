4ec916d0448b9f25134565a75078911d4759e01f
AdrianFdz19 <126997909+AdrianFdz19@users.noreply.github.com>
2026-05-22 13:37:57 -0600

    docs: add a documentation

--- vision-ai-frontend/app/layout.tsx
+++ vision-ai-frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+//Little change
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
